d20c55ee6cd5551a1964939357a223f846d33218
Yasitha Gamage <yasithamaxs@gmail.com>
2021-03-03 09:05:48 +0530

    Identify Make Complete

--- app/src/main/java/com/livecodex/quizzler/IdentifyMakeActivity.java
+++ app/src/main/java/com/livecodex/quizzler/IdentifyMakeActivity.java
@@ -2,10 +2,14 @@ package com.livecodex.quizzler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
@@ -15,15 +19,23 @@ import java.util.Random;
 public class IdentifyMakeActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     private ImageView randomImageView;
-    private Spinner mSpinner;
+    private String spinnerCarMakes;
+//    private Button makeSubmitBtn;
+
+    private Dialog resultDialog;
+
+    private int makeTestRand;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_identify_make);
 
+        resultDialog = new Dialog(this);
+
+//        makeSubmitBtn = (Button) findViewById(R.id.makeSubmit);
         randomImageView = (ImageView) findViewById(R.id.imageView) ;
-        mSpinner = (Spinner) findViewById(R.id.makeSpinner);
+        Spinner mSpinner = (Spinner) findViewById(R.id.makeSpinner);
 
         if(mSpinner != null){
             mSpinner.setOnItemSelectedListener(this);
@@ -41,7 +53,8 @@ public class IdentifyMakeActivity extends AppCompatActivity implements AdapterVi
 
     public void randomImage(){
         Random rand = new Random();
-        String imageUri = "img_"+rand.nextInt(10);
+        int randomImageNo = rand.nextInt(10);
+        String imageUri = "img_"+ randomImageNo;
         int resource_id = getResources().getIdentifier(imageUri,"drawable", "com.livecodex.quizzler");
 
         randomImageView.setImageResource(resource_id);
@@ -49,12 +62,39 @@ public class IdentifyMakeActivity extends AppCompatActivity implements AdapterVi
 
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-        String spinnerCarMakes = adapterView.getItemAtPosition(i).toString();
+        spinnerCarMakes = adapterView.getItemAtPosition(i).toString();
         Toast.makeText(getApplicationContext(), spinnerCarMakes, Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
+
+    public void checkMakeOnSubmit(View view) {
+
+        Button makeButton = (Button) view;
+        String buttonContent = makeButton.getText().toString();
+
+        if(buttonContent.equals("Next")){
+            makeButton.setText(R.string.make_submit);
+            randomImage();
+
+        }else{
+            if(spinnerCarMakes.equals("BMW")){
+                // Showing the correct dialog
+                resultDialog.setContentView(R.layout.correctpopup);
+
+            }else{
+                resultDialog.setContentView(R.layout.wrongpopup);
+
+            }
+            resultDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+            resultDialog.show();
+            makeButton.setText(R.string.next_item);
+        }
+
+
 
     }
 }
---
+++ app/src/main/java/com/livecodex/quizzler/Selections.java
@@ -0,0 +1,64 @@
+package com.livecodex.quizzler;
+
+public class Selections {
+
+    private String carImages [] = {
+            "img_0.png", //Rolls Royce
+            "img_1.png", //Rolls Royce
+            "img_2.png", //Rolls Royce
+            "img_3.png", //Rolls Royce
+            "img_4.png", //Rolls Royce
+            "img_5.png", //Rolls Royce
+            "img_6.png", //Rolls Royce
+            "img_7.png", //Rolls Royce
+            "img_8.png", //BMW
+            "img_9.png", //BMW
+            "img_10.png" //Lamborghini
+    };
+
+    private String carImageMakes [] = {
+            "Rolls Royce",
+            "Rolls Royce",
+            "Rolls Royce",
+            "Rolls Royce",
+            "Rolls Royce",
+            "Rolls Royce",
+            "Rolls Royce",
+            "Rolls Royce",
+            "BMW",
+            "BMW",
+            "Lamborghini"
+    };
+
+    private String makes [] = {
+            "Rolls Royce",
+            "Lamborghini",
+            "BMW",
+            "Toyota",
+            "Honda"
+    };
+
+    public String[] getCarImages() {
+        return carImages;
+    }
+
+    public void setCarImages(String[] carImages) {
+        this.carImages = carImages;
+    }
+
+    public String[] getCarImageMakes() {
+        return carImageMakes;
+    }
+
+    public void setCarImageMakes(String[] carImageMakes) {
+        this.carImageMakes = carImageMakes;
+    }
+
+    public String[] getMakes() {
+        return makes;
+    }
+
+    public void setMakes(String[] makes) {
+        this.makes = makes;
+    }
+}
